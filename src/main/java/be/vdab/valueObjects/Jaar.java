75715e5175def1f55b02379f53d643c2a3bfdec5
Maarten Westelinck <maartenwestelinck@hotmail.com>
2016-12-16 23:51:45 +0100

    taak 1.4 gestart bij einde hoofdstuk 5

--- src/main/java/be/vdab/valueObjects/Jaar.java
+++ src/main/java/be/vdab/valueObjects/Jaar.java
@@ -11,6 +11,21 @@ public class Jaar {
         this.jaar = jaar;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Jaar jaar1 = (Jaar) o;
+
+        return jaar == jaar1.jaar;
+    }
+
+    @Override
+    public int hashCode() {
+        return jaar;
+    }
+
     public boolean isSchrikkeljaar() {
         return jaar % 400 == 0 || jaar % 100 != 0 && jaar % 4 == 0;
     }
--- src/test/java/be/vdab/valueObjects/JaarTest.java
+++ src/test/java/be/vdab/valueObjects/JaarTest.java
@@ -20,4 +20,19 @@ public class JaarTest {
     public void eenJaarDeelbaarDoor100IsGeenSchrikkeljaar() throws Exception {
         assertEquals(false, new Jaar(1900).isSchrikkeljaar());
     }
+
+    @Test
+    public void equalsOpTweeDezelfdeJarenMoetTrueZijn() throws Exception {
+        assertEquals(new Jaar(2015), new Jaar(2015));
+    }
+
+    @Test
+    public void equalsOpTweeVerschillendeJarenMoetFalseZijn(){
+        assertNotEquals(new Jaar(2015), new Jaar(2016));
+    }
+
+    @Test
+    public void hashCodeOpTweeDezelfdeJarenMoetGelijkZijn(){
+        assertEquals(new Jaar(2015).hashCode(), new Jaar(2015).hashCode());
+    }
 }
