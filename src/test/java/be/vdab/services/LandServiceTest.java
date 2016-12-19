10e1dd0e1061d9eb2b7e2cfd13c9414f46f5d6b6
Maarten Westelinck <maartenwestelinck@hotmail.com>
2016-12-19 11:35:33 +0100

    Oefening 1.6 afgewerkt

--- src/test/java/be/vdab/services/LandServiceTest.java
+++ src/test/java/be/vdab/services/LandServiceTest.java
@@ -1,6 +1,9 @@
 package be.vdab.services;
 import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
+import static org.mockito.Mockito.*;
+
+import be.vdab.entities.Land;
 import org.junit.Before;
 import org.junit.Test;
 import be.vdab.repositories.LandRepository;
@@ -9,11 +12,15 @@ public class LandServiceTest {
     private LandRepository landRepository;
     private LandService landService;
     @Before public void before() {
-        landRepository = new LandRepositoryStub();
+        landRepository = mock(LandRepository.class);
+        when(landRepository.findOppervlakteAlleLanden()).thenReturn(20);
+        when(landRepository.read("B")).thenReturn(new Land("B", 5));
         landService = new LandService(landRepository);
     }
     @Test public void findVerhoudingOppervlakteLandTovOppervlakteAlleLanden() {
         assertEquals(0, BigDecimal.valueOf(0.25).compareTo(
                 landService.findVerhoudingOppervlakteLandTovOppervlakteAlleLanden("B")));
+        verify(landRepository).findOppervlakteAlleLanden();
+        verify(landRepository).read("B");
     }
 }
--- src/test/java/be/vdab/services/WinstServiceTest.java
+++ src/test/java/be/vdab/services/WinstServiceTest.java
@@ -4,6 +4,7 @@ import be.vdab.repositories.KostRepository;
 import be.vdab.repositories.KostRepositoryStub;
 import be.vdab.repositories.OpbrengstRepository;
 import be.vdab.repositories.OpbrengstRepositoryStub;
+import static org.mockito.Mockito.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,11 +19,15 @@ import static org.junit.Assert.*;
 public class WinstServiceTest {
 
     private WinstService w;
+    private KostRepository k;
+    private OpbrengstRepository o;
 
     @Before
     public void before() {
-        KostRepository k = new KostRepositoryStub();
-        OpbrengstRepository o = new OpbrengstRepositoryStub();
+        k = mock(KostRepository.class);
+        o = mock(OpbrengstRepository.class);
+        when(k.findTotaleKost()).thenReturn(BigDecimal.valueOf(20));
+        when(o.findTotaleOpbrengst()).thenReturn(BigDecimal.valueOf(50));
         w = new WinstService(o, k);
     }
 
@@ -30,6 +35,8 @@ public class WinstServiceTest {
     public void opbrengst50Min20Is30() {
         BigDecimal winst = w.getWinst();
         assertEquals(20.0, winst.doubleValue(), 0.0);
+        verify(k).findTotaleKost();
+        verify(o).findTotaleOpbrengst();
     }
 
 }
